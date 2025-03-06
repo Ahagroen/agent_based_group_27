1742f0735ae022e2f00659f49408f20d78fced50
Ahagroen <xandergroen@gmail.com>
2025-03-06 16:52:46 +0100

    added docstrings to ground control

--- src/ground_control.py
+++ src/ground_control.py
@@ -3,8 +3,26 @@ class groundControl():
     def __init__(self,nodes):
         self.nodes = nodes
     def determine_route(self,start_pos,end_pos)->list[int]:
+        """Determine the route using a pathfinding algorithm - If needed it can have access to all other paths through state
+
+        Args:
+            start_pos (int): starting node
+            end_pos (int): ending node
+
+        Returns:
+            list[int]: list of nodes to travel to
+        """
         #The main pathfinding algorithm - TODO
         pass
-    def determine_next_wait_position(self,start_pos,loading_ac)->int:
-        #scheduler TODO - 
+    def determine_next_wait_position(self,start_pos:int,loading_ac:list)->int:
+        """Determine where aircraft should wait/pick up the next aircraft
+
+        Args:
+            start_pos (int): node starting
+            loading_ac (list): current ac loading state
+
+        Returns:
+            int: end node to wait
+        """
+        #scheduler TODO - where do tugs wait for aircraft
         pass

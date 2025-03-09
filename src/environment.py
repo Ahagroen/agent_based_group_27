01e89f8cb94e525e11c8d6856df3076b6f612ad2
Ahagroen <xandergroen@gmail.com>
2025-03-09 11:25:09 +0100

    updated json to include positions

--- src/environment.py
+++ src/environment.py
@@ -1,11 +1,12 @@
 import json
 from typing import Union
+import openpyxl
 
-from src.datatypes import Aircraft
+from datatypes import Aircraft
 class Airport():
     def __init__(self,airport_file):
-        self.load_airport_data(airport_file)
-    def load_airport_data(self,airport_file):
+        self._load_airport_data(airport_file)
+    def _load_airport_data(self,airport_file):
         with open(airport_file,"r") as fs:
             data = json.load(fs)
             node_map = data["nodes"]
@@ -20,10 +21,7 @@ class Airport():
             self.arrival_runways = data["arrival_runways"]
             self.gates = data["gates"]
             self.nodes = node_map
-            
-    def save_airport_data(self,airport_file):
-        with open(airport_file,"w") as fs:
-            json.dump(self.data,fs)
+ 
 
     def populate_waiting_dict(self)->dict[str,Union[Aircraft|None]]:
         carry = {}
@@ -35,8 +33,13 @@ class Airport():
             carry[i] = None
         return carry
 
-
-
 if __name__ == "__main__":
-    Airport("baseline_airport_1.json").save_airport_data("baseline_airport.json")
-            
+    with open("baseline_airport.json","r") as fs:
+        data = json.load(fs)
+    xlsx_data = openpyxl.load_workbook("nodes.xlsx")
+    updated_nodes = {}
+    for i in data["nodes"].keys():
+        updated_nodes[i] = {"edges":data["nodes"][i],"x_pos":xlsx_data.active.cell(int(i)+1,2).value,"y_pos":xlsx_data.active.cell(int(i)+1,3).value}
+    data["nodes"] = updated_nodes
+    with open("baseline_airport.json","w") as fs:
+        json.dump(data,fs)
--- src/visulization.py
+++ src/visulization.py
@@ -1,11 +1,16 @@
-from raylib import BeginDrawing, InitWindow, SetTargetFPS, WindowShouldClose
+from raylib import BeginDrawing, InitWindow, SetTargetFPS, WindowShouldClose,DrawCircle
 from src.simulation import Simulation
 from src.environment import Airport
 
 InitWindow(1920,1080,b"AutoTaxi Simulation")
 SetTargetFPS(24)
-sim = Simulation(2,Airport.load_airport_data("baseline_airport.json"),15,10,45,1080)
+airport = Airport("baseline_airport.json")
+sim = Simulation(2,airport,15,10,45,1080)
+debug = True
 while not WindowShouldClose():
     BeginDrawing()
+    if debug:
+        for i in airport.nodes:
+            DrawCircle()
     position_data = sim.position_list()
     

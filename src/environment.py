004ae5825d0117f9268b05c22f146d137bf41e1b
Ahagroen <xandergroen@gmail.com>
2025-03-09 11:26:08 +0100

    removed openpyxl from dependancies

--- src/environment.py
+++ src/environment.py
@@ -1,6 +1,5 @@
 import json
 from typing import Union
-import openpyxl
 
 from datatypes import Aircraft
 class Airport():
@@ -33,13 +32,3 @@ class Airport():
             carry[i] = None
         return carry
 
-if __name__ == "__main__":
-    with open("baseline_airport.json","r") as fs:
-        data = json.load(fs)
-    xlsx_data = openpyxl.load_workbook("nodes.xlsx")
-    updated_nodes = {}
-    for i in data["nodes"].keys():
-        updated_nodes[i] = {"edges":data["nodes"][i],"x_pos":xlsx_data.active.cell(int(i)+1,2).value,"y_pos":xlsx_data.active.cell(int(i)+1,3).value}
-    data["nodes"] = updated_nodes
-    with open("baseline_airport.json","w") as fs:
-        json.dump(data,fs)

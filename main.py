5011a0e8d291b223b97186377c03b8a0d005427b
Ahagroen <xandergroen@gmail.com>
2025-03-23 11:40:27 +0100

    working V1 of ants

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from src.environment import Airport
 from src.visulization import Run_visualization  # noqa: F401
 from src.simulation import run_simulation
+from src.ants import compute_schedule
 
 
 
 def main():
     airport = Airport("baseline_airport.json")
-    run_simulation(2,airport,1080*60,15*60,10*60,45*60)
+    # run_simulation(2,airport,1080*60,15*60,10*60,45*60)
+    compute_schedule(airport,2)
 
 if __name__ == "__main__":
     main()

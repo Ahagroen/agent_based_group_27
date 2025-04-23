60ff7dbadf58b46e3d99db765e0241bde5304999
jlourof <jlourofuentes@gmail.com>
2025-04-23 17:07:50 +0200

    input documentation of main.py

--- main.py
+++ main.py
@@ -13,25 +13,41 @@ def simulate_data(runs:int):
     airport = Airport("baseline_airport.json")
     carry = []
     for _ in range(runs):
-        result = run_simulation(airport,480*60,10*60,20*60,30*60, Schedule_Algo.greedy, 42)
+
+        run_time = 8 * 60 * 60             # Length of simulation                           # [s]
+        ac_freq = 10 * 60                  # Frequency of aircraft arrival                  # [s]
+        taxi_margin = 20 * 60              # Time margin for aircraft to arrive at gate     # [s]
+        loading_time = 30 * 60             # Time spent by aircraft at gate                 # [s]
+        scheduler = Schedule_Algo.greedy   # Time of algorith used to manage tug schedule   # [-]
+        rng_seed = 42                      # Seed used to generate random variables         # [-]
+
+        result = run_simulation(airport, run_time, ac_freq, taxi_margin, loading_time, scheduler, rng_seed)
         carry.append(result)
-    print(f"num successes: {[x[0] for x in carry].count(Status.Success)}, percentage = {[x[0] for x in carry].count(Status.Success)/len(carry)}")
+
+    print(f"num successes: {[x[0] for x in carry].count(Status.Success)},"
+          f" percentage = {[x[0] for x in carry].count(Status.Success)/len(carry)}")
 
 def main():
 
     # Simulation Inputs
     # -----------------
-    x_dim = 900
-    y_dim = 780
-    fps = 120
-    run_time = 8 * 60 * 60
-    ac_freq = 10 * 60
-    taxi_margin = 20 * 60
-    loading_time = 30 * 60
-    scheduler = Schedule_Algo.greedy
-    rng_seed = 42
+    x_dim = 900                        # Width of window                                # [Pixels]
+    y_dim = 780                        # Height of window                               # [Pixels]
+    fps = 120                          # Frames per second                              # [-]
+    run_time = 8 * 60 * 60             # Length of simulation                           # [s]
+    ac_freq = 10 * 60                  # Frequency of aircraft arrival                  # [s]
+    taxi_margin = 20 * 60              # Time margin for aircraft to arrive at gate     # [s]
+    loading_time = 30 * 60             # Time spent by aircraft at gate                 # [s]
+    scheduler = Schedule_Algo.greedy   # Time of algorith used to manage tug schedule   # [-]
+    rng_seed = 42                      # Seed used to generate random variables         # [-]
 
+    # Run Simulations or Visualization
+    # --------------------------------
+
+    # Visualization
     Run_visualization(x_dim, y_dim, fps, run_time, ac_freq, taxi_margin, loading_time, scheduler, rng_seed)
+
+    # Simulation
     # simulate_data(25)
 
 if __name__ == "__main__":
